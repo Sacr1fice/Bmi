115f66e9b875ebec1b4a32888957d160963adee3
Sacr1fice <sanunkurov@gmail.com>
2023-06-09 15:58:26 +0300

    rework

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,9 @@
+import static java.lang.Math.pow;
+
 public class BmiService {
     public float calculate(double weight, double height) {
         int bmi;
-        bmi = (int)(weight / (height * height));
+        bmi = (int)(weight / pow(height, 2));
         return bmi;
     }
 }
